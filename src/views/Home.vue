6d5179208ff07225f0c2df3415934a404d8df723
guiller0300 <guiller0300@gmail.com>
2021-05-05 15:02:44 -0500

    rsockets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,11 +42,14 @@
   </div>
 </template>
 <script>
+
+import { RSocketClient, JsonSerializers } from "rsocket-core";
+import RSocketWebSocketClient from "rsocket-websocket-client";
 export default {
   name: "websocketdemo",
   data() {
     return {
-      websocketUrl: "ws://localhost:8091/greetings",
+      websocketUrl: "ws://localhost:8080/rsocket",
       received_messages: [],
       send_message: null,
       connected: false
@@ -61,7 +64,40 @@ export default {
         this.socket.send(JSON.stringify(msg));
       }
     },
-    connect() {
+connect() {
+      console.log("connecting with RSocket...");
+      const transport = new RSocketWebSocketClient({
+        url: "ws://localhost:8801"
+      });
+      const client = new RSocketClient({
+        // send/receive JSON objects instead of strings/buffers
+        serializers: JsonSerializers,
+        setup: {
+          // ms btw sending keepalive to server
+          keepAlive: 60000,
+          // ms timeout if no keepalive response
+          lifetime: 180000,
+          // format of `data`
+          dataMimeType: "application/json",
+          // format of `metadata`
+          metadataMimeType: "application/json"
+        },
+        transport
+      });
+      client.connect().subscribe({
+        onComplete: socket => {
+          this.socket = socket;
+        },
+        onError: error => {
+          console.log("got connection error");
+          console.error(error);
+        },
+        onSubscribe: cancel => {
+          /* call cancel() to abort */
+        }
+      }),
+},
+    /*connect() {
       this.socket = new WebSocket(this.websocketUrl);
       this.socket.onopen = () => {
         console.log("connected to " + this.websocketUrl);
@@ -80,7 +116,7 @@ export default {
         this.connected = false;
         console.log(error);
       };
-    },
+    },*/
     disconnect() {
       console.log("trying to disconnect..");
       this.socket.close();
